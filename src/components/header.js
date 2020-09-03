af47c6e7ddca816ed760f86520b4e68f02fd3cc2
R <50966008+rashmika13@users.noreply.github.com>
2020-09-02 20:32:28 -0400

    Gatsby website started

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#c53357`,
       marginBottom: `1.45rem`,
     }}
   >
